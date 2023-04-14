9bd7f71b2c66d1740ce67eb5e11a415ac2bd25c6
Mike <80673026+mssj-11@users.noreply.github.com>
2023-04-14 16:10:43 -0600

    Spring Boot - MongoDB - CRUD API Restful

---
+++ src/test/java/com/mss/springboot/web/app/SpringBootMongodbCrudApiRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mss.springboot.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongodbCrudApiRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
